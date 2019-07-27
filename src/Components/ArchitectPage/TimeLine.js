e09fb79dbff55e0c7a53c9c98ecd62088b0b7e40
ins21 <leonnby@gmail.com>
2019-07-27 12:56:06 +0300

    feat: implement TimeLine component

---
+++ src/Components/ArchitectPage/TimeLine.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
+
+const TimeLine = (props) => {
+  const items = props.data.map((item, index) => (
+    <TimelineItem
+    key={index}
+    dateText={item.date}
+    dateInnerStyle={{ background: '#76bb7f', color: '#000' }}
+    bodyContainerStyle={{
+      background: '#ddd',
+      padding: '20px',
+      borderRadius: '8px',
+      boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
+    }}
+    style={{ color: '#e86971' }}
+  >
+    <p>{item.text}</p>
+  </TimelineItem>
+  ))
+
+  return (
+    <Timeline lineColor={'#ddd'}>{items}</Timeline>
+  )
+}
+
+export default TimeLine
--- src/Components/Architectors/Architect.js
+++ src/Components/Architectors/Architect.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Typography  } from '@material-ui/core'
+import TimeLine from '../ArchitectPage/TimeLine'
 
 function Architect(props) {
     return <> 
@@ -7,7 +8,9 @@ function Architect(props) {
             <img src={props.image} alt="Фото" style={{height: '300px'}}></img>
             <Typography variant="h4" component="h2" style={{margin: 10}}>{props.name}</Typography>
             <Typography variant="h6" style={{marginBottom: 10}}>Годы жизни: {props.born} - {props.deceased}</Typography>
-            <Typography paragraph={true} style={{marginBottom: 10}}>{props.description}</Typography>
+            <Typography paragraph={true} style={{marginBottom: 30}}>{props.description}</Typography>
+            <Typography variant="h4" component="h4">Биография</Typography>
+            <TimeLine data={props.bio}/>
         </Container>
     </>
 }
