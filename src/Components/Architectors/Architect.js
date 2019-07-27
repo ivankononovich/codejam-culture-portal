a6edd96ec192a6ca57752d015549ba7900d42058
ins21 <leonnby@gmail.com>
2019-07-27 12:42:57 +0300

    feat: add basic styles for architect`s page

--- src/Components/Architectors/Architect.js
+++ src/Components/Architectors/Architect.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Container, Typography  } from '@material-ui/core'
 
 function Architect(props) {
     return <> 
-        <h2>{props.name}</h2>
-        <span>Годы жизни {props.born} - {props.deceased}</span>
-        <p>{props.description}</p>
+        <Container style={{textAlign: 'center'}} maxWidth="md" justify="center">
+            <img src={props.image} alt="Фото" style={{height: '300px'}}></img>
+            <Typography variant="h4" component="h2" style={{margin: 10}}>{props.name}</Typography>
+            <Typography variant="h6" style={{marginBottom: 10}}>Годы жизни: {props.born} - {props.deceased}</Typography>
+            <Typography paragraph={true} style={{marginBottom: 10}}>{props.description}</Typography>
+        </Container>
     </>
 }
 
