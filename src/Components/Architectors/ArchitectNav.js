3ed81debfee975b40ef528860c55a5a8c3c6d68d
Ivan Kononovich <fogfug1@gmail.com>
2019-07-27 11:53:12 +0300

    fix: hide the list of architects when you go to the page of a particular architect

--- src/Components/Architectors/ArchitectNav.js
+++ src/Components/Architectors/ArchitectNav.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { architects } from '../../store';   
-import Architect from './Architect';
 
 function findAllName() {
     const names = [];
@@ -24,38 +23,17 @@ function createNav() {
 
     links.forEach((item, index) => {
         nav.push(<li key={index} data-index={index}>
-            <Link to={`/architects/${item.url}`}>{item.name}</Link>
+            <Link to={`/${item.url}`}>{item.name}</Link>
         </li>)
     });
 
     return nav;
 }
 
-function createRouter() {
-    const links = findAllName();
-    const routers = [];
-
-    links.forEach((item) => {
-        const architect = architects[item.index];
-        
-        routers.push(
-            <Route 
-                key={item.url}
-                path={`/architects/${item.url}`}
-
-                render={() => <Architect {...architect}/>}
-            />
-        )
-    });
-
-    return routers;
-}
-
 function ArchitectNav() {
     return <>
         <ul>
             {createNav()}
-            {createRouter()}
         </ul>
     </>
 }
--- src/Components/GlobalNav.js
+++ src/Components/GlobalNav.js
@@ -3,7 +3,44 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import PortalDescription from './HomePage/PortalDescription';
 import Autors from './AutorsPage/Autors';
+import Architect from './Architectors/Architect';
 import ArchitectNav from './Architectors/ArchitectNav';
+import { architects } from '../store';   
+
+
+function findAllName() {
+    const names = [];
+
+    architects.forEach((item, index) => {
+        names.push({
+            url: item.id,
+            name: item.name,
+            index
+        });
+    });
+
+    return names;
+}
+
+function createRouter() {
+    const links = findAllName();
+    const routers = [];
+
+    links.forEach((item) => {
+        const architect = architects[item.index];
+        
+        routers.push(
+            <Route exact
+                key={item.url}
+                path={`/${item.url}`}
+
+                render={() => <Architect {...architect}/>}
+            />
+        )
+    });
+
+    return routers;
+}
 
 function GlobalNav() {
     return <>
@@ -19,14 +56,16 @@ function GlobalNav() {
                 path="/"
                 render={() => <PortalDescription />}
             />
-            <Route 
+            <Route exact
                 path="/autors"
                 render={() => <Autors />}
             />
-            <Route 
+            <Route exact
                 path="/architects"
                 render={() => <ArchitectNav />}
             />
+
+            { createRouter() }
         </Router>
     </>
 }
