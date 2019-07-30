e2fbcd636ecdb7090e8bdd8e2f55285149f44ed6
Pavel Khomich <pashax0@mail.ru>
2019-07-30 15:29:35 +0300

    refactor: change mapping of architectors

--- src/Components/Navigation/ArchitectNav.js
+++ src/Components/Navigation/ArchitectNav.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
 
-function createNav(props) {
-    const nav = [];
+const useStyles = makeStyles({
+    card: {
+      maxWidth: '90vw',
+    },
+});
 
-    props.links.forEach((item, index) => {
-        nav.push(<li key={index} data-index={index}>
-            <Link to={`/${item.url}`}>{item.name}</Link>
-        </li>)
-    });
-
-    return nav;
-}
-
-function ArchitectNav(props) {
+export default (props) => {
+    const architects = props.links;
+    const classes = useStyles();
     return <>
-        <ul>
-            {createNav(props)}
+        <ul >
+        {architects.map((architector, i) =>
+            <li
+                style={{listStyle: 'none'}}
+                key={i}
+            >
+                <Link
+                    to={`/${architector.url}`}
+                >
+                    <Card className={classes.card}>
+                    {architector.name}
+                    </Card>
+                </Link>
+            </li>)}
         </ul>
     </>
 }
-
-export default ArchitectNav;
