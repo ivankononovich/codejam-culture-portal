3defdb1b8f29a48ba60d7a4597c8b26079f0dc45
Pavel Khomich <pashax0@mail.ru>
2019-08-01 20:34:37 +0300

    feat: add developers description in footer

--- src/Components/HomePage/Footer.js
+++ src/Components/HomePage/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Container, GridListTile, GridList, Popover, Typography } from '@material-ui/core';
+import { AppBar, Container, GridListTile, GridList, Popover, Typography, Avatar, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -12,78 +12,95 @@ const useStyles = makeStyles(theme => ({
         display: 'flex',
         justifyContent: 'space-around',
     },
+    wrapper: {
+      padding: '0.5rem',
+    },
     list: {
-
+      cursor: 'pointer',
     },
-    popover: {
-      pointerEvents: 'none',
-    },
-    paper: {
-      padding: theme.spacing(1),
+    avatar: {
+      margin: '0 auto',
     },
   }));
 
 export default (props) => {
     const devSort = props.developers.sort((a, b) => {return a.name > b.name ? 1 : -1} );
-
     const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorEl, setAnchorEl] = React.useState({anchor: null, dev: null});
 
-    function handlePopoverOpen(event) {
-      setAnchorEl(event.currentTarget);
+    function handlePopoverOpen(event, devId) {
+      const dev = devSort.filter(developer => developer.id === devId)[0];
+      setAnchorEl({anchor: event.currentTarget, dev: dev});
     }
 
     function handlePopoverClose() {
-      setAnchorEl(null);
+      setAnchorEl({anchor: null, dev: null});
     }
 
-    const open = Boolean(anchorEl);
-
+    const open = Boolean(anchorEl.anchor);
+    // const id = open ? 'simple-popover' : undefined;
+    
     return (
     <>
     <AppBar position="fixed" color="primary" className={classes.appBar}>
         <Container className={classes.container}>
-            {/* {`${props.devTitle}: `} */}
-            <GridList cols={7} style={{justifyContent: 'space-between'}}>
-            {devSort.map((dev, i) =>
+            <GridList style={{justifyContent: 'space-between'}}>
+              {devSort.map((dev, i) =>
                 <GridListTile
-                    key={i}
-                    style={{width: 'auto', height: 'auto', margin: 'auto 1rem'}}
+                  key={i}
+                  style={{width: 'auto', height: 'auto', margin: 'auto 1rem'}}
                 >
-                    <Typography
-                        aria-owns={open ? 'mouse-over-popover' : undefined}
-                        aria-haspopup="true"
-                        onMouseEnter={handlePopoverOpen}
-                        onMouseLeave={handlePopoverClose}
-                    >
+                  <Typography
+                    className={classes.list}
+                    variant="h6"
+                    component="h3"
+                    onMouseOver={(ev) => handlePopoverOpen(ev, dev.id)}
+                  >
                     {`@${dev.github.match(/([^/]+$)/g)}`}
-                    </Typography>
-                    <Popover
-                      id="mouse-over-popover"
-                      className={classes.popover}
-                      classes={{
-                        paper: classes.paper,
-                      }}
-                      open={open}
-                      anchorEl={anchorEl}
-                      anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                      }}
-                      transformOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                      }}
-                      onClose={handlePopoverClose}
-                      disableRestoreFocus
-                    >
-                      <Typography>
-                          {dev.name}
-                      </Typography>
-                    </Popover>
+                  </Typography>
                 </GridListTile>
-            )}
+              )}
             </GridList>
+            <Popover
+              className={classes.popover}
+              open={open}
+              anchorEl={anchorEl.anchor}
+              onClose={handlePopoverClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+            >
+              <div
+                onMouseLeave={handlePopoverClose}
+                className={classes.wrapper}
+              >
+                <Avatar
+                  className={classes.avatar}
+                  alt={anchorEl.dev ? anchorEl.dev.name : ''}
+                  src= {anchorEl.dev ? anchorEl.dev.image : ''}
+                />
+                <Typography variant="h5" component="h4">
+                  {anchorEl.dev ? anchorEl.dev.name : ''}
+                </Typography>
+                <Typography
+                  variant="h6"
+                  component="h6"
+                >
+                 {`Github: `}
+                <Link
+                  href={anchorEl.dev ? anchorEl.dev.github : ''}
+                  target='_blank'
+                >
+                  {anchorEl.dev ? `${anchorEl.dev.github.match(/([^/]+$)/g)}` : ''}
+                  </Link>
+                </Typography>
+              </div>
+            </Popover>  
         </Container>
         </AppBar>
     </>
